8188a0c240d752abe04b0eaa249e94d01a8c5109
EKO FEBRIANTO RAMADHAN <86284651+Febriantoekor78@users.noreply.github.com>
2021-06-22 14:30:25 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<center>
+		<footer>
+		<p>Jejak Si Gundul ---------2019</p>	
+		</footer>
+</center>
+
---
+++ title_index.php
@@ -0,0 +1,17 @@
+				
+	<div class="row-fluid">
+			<div class="span12"></div>
+				  <div class="row-fluid">
+						<div class="span10">
+						<img class="index_logo" src="admin/images/sclogo.png">
+						</div>	
+						<div class="span12">
+							<div class="motto">
+							<br>
+							<p>WELCOME&nbsp;&nbsp;TO:</p>
+							<p>Sistem Informasi Inventori Stok Barang Dan Display</p>												
+							</div>											
+						</div>							
+				  </div>		   							
+    </div>	
+				
